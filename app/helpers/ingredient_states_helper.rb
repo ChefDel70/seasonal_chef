776075e7be86bdaa82066bc8bbbc08068019315f
Josh Del Sesto <Chefdel70@gmail.com>
2017-11-10 13:17:32 -0500

    Generated Controller, views, and routes for our views

---
+++ app/helpers/ingredient_states_helper.rb
@@ -0,0 +1,2 @@
+module IngredientStatesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	root 'ingredient_states#index'
+	get 'show' => 'ingredient_states#show'
+	get 'search_results' => 'ingredient_states#search_results'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
