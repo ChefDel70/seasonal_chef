a505ca48ccaabb4c8899e275118ec8bcc92e6344
Nick Kautz <Nick.Kautz@gmail.com>
2017-11-12 12:00:27 -0500

    updated views, consolidated stylesheets and incoporated responsive design

--- app/controllers/ingredient_states_controller.rb
+++ app/controllers/ingredient_states_controller.rb
@@ -1,18 +1,18 @@
 class IngredientStatesController < ApplicationController
 
 	def index
-		@states = State.all 
-	end 
+		@states = State.all
+	end
 
 	def show
 		@ingredients = Ingredient.all
-	end 
+	end
 
 	def search_results
-	end 
+	end
 
 	def ingredients_in_season_now
 		all_matches = self.ingredients
 		all_matches
-	end 
+	end
 end
