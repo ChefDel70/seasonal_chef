9a1945b71b37f762c394166125747ebeff46a0ee
Nick Kautz <Nick.Kautz@gmail.com>
2017-11-12 16:40:49 -0500

    modified routes, added ingredients view and archived previous ingredients view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 	root 'recipes#index'
 	get 'show' => 'ingredient_states#show'
 	get 'search_results' => 'ingredient_states#search_results'
-	get 'hoho' => 'ingredient_states#index'
+	get 'ingredients' => 'recipes#ingredients'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
