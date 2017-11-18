adb24cfb56a79920477e2307a9b753965ee2d963
Nick Kautz <Nick.Kautz@gmail.com>
2017-11-18 16:11:42 -0500

    responsive design edits and updated controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,29 +6,28 @@ class RecipesController < ApplicationController
 
 	def ingredients
 
-		@state = State.find(params[:state_id])
-		@ingredients = @state.ingredients.where(
-			ingredient_states: {
-				in_season_week: Time.now.strftime("%U").to_i
-			}
-		).joins(:ingredient_states).distinct
+
 		if params[:state_id].empty?
 			redirect_to root_path
 		else
 			 @state = State.find(params[:state_id])
-			 @ingredients = @state.ingredients
+			 @ingredients = @state.ingredients.where(
+			ingredient_states: {
+				in_season_week: Time.now.strftime("%U").to_i
+			}
+		).joins(:ingredient_states).distinct
 		end
 	end
 
-  def recipes
-  	ingredients = params[:ingredients]
-  	first_ingredient = ingredients[0]
-  	other_ingredients = ingredients.last(ingredients.length - 1).join(", ")
-  	if params[:ingredients].length == 1
-  	    @recipes = RecipePuppy::Recipe.search_for(first_ingredient).get['results']
-	else
-		@recipes = RecipePuppy::Recipe.search_for(first_ingredient).with_ingredients(other_ingredients).get['results']
-  	end
-  end
+	def recipes
+	  	ingredients = params[:ingredients]
+	  	first_ingredient = ingredients[0]
+	  	other_ingredients = ingredients.last(ingredients.length - 1).join(", ")
+	  	if params[:ingredients].length == 1
+	  	    @recipes = RecipePuppy::Recipe.search_for(first_ingredient).get['results']
+		else
+			@recipes = RecipePuppy::Recipe.search_for(first_ingredient).with_ingredients(other_ingredients).get['results']
+	  	end
+	  end
 
 end
